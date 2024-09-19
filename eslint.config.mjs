db7f34ebd245cb8602f3230bb7b116e35cf3ec8c
dariojoel0905 <dariojoel0905@gmail.com>
2024-09-19 11:09:47 +0200

    formatter test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
 export default [
   {
-    ignores: ['node_modules/**'], // Ignoriert node_modules-Verzeichnis
+    ignores:
+      [
+        'node_modules/**',
+      ], // Ignoriert node_modules-Verzeichnis
   },
   {
-    rules: {
-      'no-unused-vars': 'error',  // Fehlermeldung, wenn eine Variable deklariert, aber nicht verwendet wird
-      'prefer-const': 'error',    // Fehlermeldung, wenn 'let' anstelle von 'const' verwendet wird
-      'eqeqeq': 'error'           // Fehlermeldung, wenn '==' statt '===' verwendet wird
-    },
+    rules:
+      {
+        'no-unused-vars':
+          'error', // Fehlermeldung, wenn eine Variable deklariert, aber nicht verwendet wird
+        'prefer-const':
+          'error', // Fehlermeldung, wenn 'let' anstelle von 'const' verwendet wird
+        eqeqeq:
+          'error', // Fehlermeldung, wenn '==' statt '===' verwendet wird
+      },
   },
 ];
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import chalk from 'chalk';
 
-const  myUnusedVariable = 1;
+const myUnusedVariable = 1;
 
-if (myUnusedVariable === 1)
-{
-    console.log(chalk.blue('It works'));
+if (
+  myUnusedVariable ===
+  1
+) {
+  console.log(
+    chalk.blue(
+      'It works',
+    ),
+  );
 }
